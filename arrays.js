0d5f3a1dbeb3d1d7e887ea33e78a9804ba50d031
amazing-server-2710 <celiamacrae@gmail.com>
2019-07-08 21:12:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -14,8 +14,3 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
-
-function addElementToEndOfArray(array, element){
-  var newArray2 = [array... element]
-  return newArray2
-}
